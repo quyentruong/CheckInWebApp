00246cec4a927b7b3a2becb2f735c6a1e50eb4c8
Quyen Truong <quyen.truong0202@gmail.com>
2018-07-31 20:44:23 -0700

    date filter

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 import {Template} from 'meteor/templating';
-import {CheckStatus} from "../lib/collections";
+import {CheckStatus, publicStatus} from "../lib/collections";
 import {Accounts} from 'meteor/accounts-base';
 import './main.html';
 import {Meteor} from "meteor/meteor";
+import {ReactiveVar} from 'meteor/reactive-var';
 
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_ONLY'
@@ -11,21 +12,31 @@ Accounts.ui.config({
 
 Template.main.onCreated(function mainOnCreated() {
     Meteor.subscribe('checkStatuses');
+    Meteor.subscribe('publicStatuses');
+    this.date = new ReactiveVar(moment().format("MM/DD/YYYY"));
 });
 
+
 Template.main.helpers({
-    checkStatuses() {
-        return CheckStatus.find({}, {sort: {createdAt: -1}});
-    },
     ownerStatuses() {
-        return CheckStatus.find({owner: Meteor.userId()}, {sort: {createdAt: -1}});
+        return CheckStatus.find({
+            owner: Meteor.userId(),
+            time: {$regex: Template.instance().date.get().split(" ")[0]}
+        }, {sort: {createdAt: -1}});
+    },
+    publicS() {
+        return publicStatus.find({time: {$regex: Template.instance().date.get().split(" ")[0]}}, {sort: {createdAt: -1}});
     }
 
 });
 
 Template.currentStatus.helpers({
     getCurrentStatus() {
-        const last = CheckStatus.findOne({owner: Meteor.userId()}, {fields: {_id: 1, text: 1}, sort: {createdAt: -1}, limit: 1});
+        const last = CheckStatus.findOne({owner: Meteor.userId()}, {
+            fields: {_id: 1, text: 1},
+            sort: {createdAt: -1},
+            limit: 1
+        });
         if (last !== undefined) {
             Meteor.call('checkStatuses.setPublic', last._id, false);
             // console.log(last);
@@ -39,12 +50,13 @@ function insert(status) {
     if (status === "Out") {
         text = "Out";
     }
-    const time = moment().format("YYYY-MM-DD HH:mm:ss");
-    let last = CheckStatus.findOne({owner: Meteor.userId()}, {fields: {_id: 1}, sort: {createdAt: -1}, limit: 1});
-    if (last !== undefined) {
-        last = last._id;
-    }
-    Meteor.call('checkStatuses.insert', this._id, text, time, last);
+    const time = moment().add(1, "day").format("MM/DD/YYYY HH:mm:ss");
+    // let last = CheckStatus.findOne({owner: Meteor.userId()}, {fields: {_id: 1}, sort: {createdAt: -1}, limit: 1});
+    // if (last !== undefined) {
+    //     last = last._id;
+    // }
+    Meteor.call('checkStatuses.insert', this._id, text, time);
+    Meteor.call('publicStatuses.insert', this._id, text, time);
 }
 
 Template.main.events({
@@ -55,6 +67,11 @@ Template.main.events({
     'submit .add-checkOut'(event) {
         event.preventDefault();
         insert("Out");
+    },
+    'submit .filter'(event, instance) {
+        event.preventDefault();
+        let d = event.target.date.value;
+        instance.date.set(d);
     }
 });
 
@@ -74,3 +91,43 @@ Template.checkStatus.helpers({
         return this.owner === Meteor.userId();
     }
 });
+
+Template.main.rendered = function () {
+    Tracker.autorun(() => {
+        let first = null, last = null;
+        if (Meteor.userId()) {
+            first = CheckStatus.findOne({owner: Meteor.userId()}, {
+                fields: {time: 1},
+                sort: {createdAt: 1},
+                limit: 1
+            });
+            last = CheckStatus.findOne({owner: Meteor.userId()}, {
+                fields: {time: 1},
+                sort: {createdAt: -1},
+                limit: 1
+            });
+        } else {
+            first = publicStatus.findOne({}, {
+                fields: {time: 1},
+                sort: {createdAt: 1},
+                limit: 1
+            });
+            last = publicStatus.findOne({}, {
+                fields: {time: 1},
+                sort: {createdAt: -1},
+                limit: 1
+            });
+        }
+        if (first && last) {
+            this.$('.datepicker').datepicker({
+                startDate: first.time.split(" ")[0],
+                endDate: last.time.split(" ")[0],
+                todayHighlight: true
+            });
+        }
+    });
+
+
+    this.$('.datepicker').val(moment().format("MM/DD/YYYY"));
+
+};
--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 import '../lib/collections';
-import {CheckStatus} from "../lib/collections";
+import {CheckStatus, publicStatus} from "../lib/collections";
 
 Meteor.publish('checkStatuses', function checkPublication() {
     return CheckStatus.find({
@@ -11,13 +11,44 @@ Meteor.publish('checkStatuses', function checkPublication() {
     });
 });
 
+Meteor.publish('publicStatuses', function checkPublication() {
+    return publicStatus.find();
+});
+
 Meteor.methods({
+    'publicStatuses.insert'(id, text, time) {
+        const userPublic = publicStatus.find(
+            {
+                owner: Meteor.userId(),
+                time: {$regex: time.split(" ")[0]}
+            }, {sort: {createdAt: -1}}).fetch();
+        if (userPublic.length === 0) {
+            publicStatus.insert({
+                text,
+                time,
+                createdAt: new Date(),
+                owner: Meteor.userId(),
+                username: Meteor.user().username
+            });
+        } else {
+            publicStatus.update({
+                owner: Meteor.userId(),
+                time: {$regex: time.split(" ")[0]}
+            }, {
+                $set:
+                    {
+                        text: text,
+                        time: time
+                    }
+            });
+        }
+    },
     'checkStatuses.insert'(id, text, time, last) {
         if (!this.userId) {
             throw new Meteor.Error('not-authorized');
         }
 
-        CheckStatus.update(last, {$set: {private: true}});
+        // CheckStatus.update(last, {$set: {private: true}});
 
         CheckStatus.insert({
             text,
@@ -25,7 +56,7 @@ Meteor.methods({
             createdAt: new Date(),
             owner: Meteor.userId(),
             username: Meteor.user().username,
-            private: true
+            // private: true
         });
 
     },
@@ -36,6 +67,30 @@ Meteor.methods({
             throw new Meteor.Error("Unauthorized");
         }
         CheckStatus.remove(id);
+
+        const userPublic = CheckStatus.findOne({owner: todo.owner},
+            {
+                sort: {createdAt: -1},
+                limit: 1
+            });
+        // console.log(userPublic);
+        if (userPublic) {
+            publicStatus.update({
+                owner: todo.owner,
+                time: {$regex: userPublic.time.split(" ")[0]}
+            }, {
+                $set:
+                    {
+                        text: userPublic.text,
+                        time: userPublic.time
+                    }
+            });
+        } else {
+            publicStatus.remove({
+                owner: todo.owner,
+                time: {$regex: todo.time.split(" ")[0]}
+            })
+        }
     },
 
     'checkStatuses.setPublic'(id, setToPublic) {
